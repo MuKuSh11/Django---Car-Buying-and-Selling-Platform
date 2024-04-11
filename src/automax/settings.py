9b6fd29d73125b3e2c4383966765d77c9773a27a
MuKuSh11 <munesh.developer@gmail.com>
2024-04-11 14:18:34 +0530

    Using env variables

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
 import os
+import environ
+from pathlib import Path
 
 from django.contrib.messages import constants as messages
+
+env = environ.Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2$wf0pn)@+lyk6hl-mmm0)p-%+f@yt6ntynzm29tx6fud*_a=o'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(env('DEBUG'))
 
 ALLOWED_HOSTS = []
 
